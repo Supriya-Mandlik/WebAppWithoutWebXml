f4c85cf96d9b83d26448f1aaa0c5af6e1cb26407
Supriya-Mandlik <158532483+Supriya-Mandlik@users.noreply.github.com>
2024-06-06 15:19:50 +0530

    commit

---
+++ WebAppWithoutWebXml/src/main/java/in/sp/back/MyServlet1.java
@@ -0,0 +1,21 @@
+package in.sp.back;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/aaa")
+public class MyServlet1 extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("I am in MyServlet1 service() Method....");
+		
+	}
+
+}
